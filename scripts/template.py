98077979a531e525dcfc8fcd6e80af0cae98390a
Youssef T <youssef.taktak@hotmail.fr>
2021-08-06 09:56:00 +0200

    Update template.py

--- scripts/template.py
+++ scripts/template.py
@@ -1,1 +1,10 @@
+class TemplateScript:
+    def __init__(self):
+        """
+        Initialize Template Script
+        """
+        self.name = cls.__name__
+        self.dcc = 'maya'
 
+    def run(self):
+      print('This is a template script')
